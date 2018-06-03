ddfb8f244cdc698a3acf96384e1774496d995db2
monjason23 <mfabico@mfabicos-MacBook-Pro.local>
2018-06-03 18:16:18 +0800

    Added /todos API and refactor mongoose

---
+++ server/models/Todo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+  text: { type: String, required: true, minlength: 1 },
+  completed: { type: Boolean, default: false },
+  completedAt: { type: Number, default: null }
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+module.exports = {
+  Todo
+};
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { mongoose } = require("./db/mongoose");
+const { User } = require("./models/User");
+const { Todo } = require("./models/Todo");
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 3000 || process.env.PORT;
+
+app.post("/todos", (httpRequest, httpResponse) => {
+  let newTodo = new Todo({
+    text: httpRequest.body.text
+  });
+
+  newTodo.save().then(
+    result => {
+      console.log(result);
+      httpResponse.status(200).send(result);
+    },
+    error => {
+      console.log(error);
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log("App is running at port 3000");
+});
